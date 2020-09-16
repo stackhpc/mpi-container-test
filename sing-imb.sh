e5c14597749f0efbc4a34f9283b7f541e1e5ea5d
Steve Brasier <steveb@stackhpc.com>
2020-09-16 10:49:30 +0100

    SUCCESS - removed SLURM_MPI_TYPE=pmix_v2

--- sing-imb.sh
+++ sing-imb.sh
@@ -8,7 +8,6 @@
 #SBATCH --error=sing-imb.err
 module load gcc/9.3.0-5abm3xg
 module load openmpi/4.0.3-qpsxmnc
-export SLURM_MPI_TYPE=pmix_v2
 export UCX_NET_DEVICES=mlx5_0:1
 
 mpirun /alaska/steveb/opt/singularity-dev/bin/singularity exec mpi-benchmarks.sif IMB-MPI1 pingpong
